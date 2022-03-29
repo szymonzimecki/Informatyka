c75ea2b32a2f8e2d65628a6dd4467311b9287bbe
Szymon Zimecki <blckus4@gmail.com>
2022-03-29 15:02:26 +0200

    Liczby_Pierwsze_Algorytm

---
+++ liczba_pierwsza_algorytm/liczba_pierwsza_algorytm.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int do_ktorej, lp, p;
+
+    cout << "Podaj do ktorej liczby chcesz sprawdzic: ";
+    cin >> do_ktorej;
+
+    bool status = true;
+
+    lp = 0;
+    p = 2;
+
+    while (lp < do_ktorej)
+    {
+        for (int d = 2; d <= p - 1; d++)
+        {
+            if (p % d == 0) {
+                status = false;
+            }
+        }
+
+        if (status == true)
+        {
+            cout << p << endl;
+            lp += 1;
+        }
+
+        status = true;
+
+        p += 1;
+
+    }
+
+
+
+    return 0;
+}
