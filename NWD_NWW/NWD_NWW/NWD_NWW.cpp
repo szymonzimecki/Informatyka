b03642bfd1308d8fde21e43d736e2dbc81dcce90
Szymon Zimecki <blckus4@gmail.com>
2022-04-05 15:00:18 +0200

    NWD&NWW

---
+++ NWD_NWW/NWD_NWW/NWD_NWW.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <chrono>
+using namespace std;
+
+int main()
+{
+    int a, b, iloczyn, nww;
+
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+
+    iloczyn = a * b;
+
+    while (a != b)
+    {
+
+        if (a < b) {
+            b -= a;
+        }
+
+        else {
+            a -= b;
+        }
+    }
+
+    nww = iloczyn / a;
+
+    cout << "Najwiekszy wspolny dzielnik: " << a;
+    cout << endl;
+
+
+    cout << "Najmniejsza wspolna wielokrotnosc: " << nww;
+
+}
