a736e5a4d1348b6cae31ea6e1e1506f4564f7d16
Szymon Zimecki <blckus4@gmail.com>
2022-04-13 13:58:00 +0200

    Szyfr Cezara

---
+++ szyfr_cezara/szyfr_cezara.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <chrono>
+using namespace std;
+
+chrono::duration<double> elapsed_seconds;
+
+void szyfrowanie(int klucz, char tab[]){
+
+    int dl = strlen(tab);
+
+    if (!(klucz >= -26 and klucz <= 26)) {
+        return;
+    }
+
+    if (klucz >= 0) {
+        for (int i = 0; i < dl; i++) {
+            if (tab[i] + klucz <= 'z') {
+                tab[i] += klucz;
+            }
+            else
+            {
+                tab[i] += (klucz - 26);
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < dl; i++) {
+            if (tab[i] + klucz >= 'a') {
+                tab[i] += klucz;
+            }
+            else
+            {
+                tab[i] += (klucz + 26);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    chrono::system_clock::time_point start = chrono::system_clock::now();	// czas start
+
+    int dl, klucz;
+    char tab[200];
+
+    cout << "Podaj klucz: ";
+    cin >> klucz;
+    cout << "Podaj wyraz: ";
+    cin >> tab;
+    cout << endl;
+
+    szyfrowanie(klucz, tab);
+    cout << "Zaszyfrowane: " << tab;
+
+    cout << endl;
+
+    szyfrowanie(-klucz, tab);
+    cout << "Odszyfrowane: " << tab << endl;
+
+
+    chrono::system_clock::time_point end = chrono::system_clock::now(); // czas stop
+    elapsed_seconds += end - start;	// obliczenie czasu
+
+    cout << std::chrono::duration_cast<std::chrono::seconds>(elapsed_seconds).count() << "\t seconds\n";
+
+
+}
